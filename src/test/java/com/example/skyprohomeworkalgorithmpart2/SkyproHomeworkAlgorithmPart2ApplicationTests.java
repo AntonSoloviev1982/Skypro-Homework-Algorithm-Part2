3d799970c478aa19180aa43bf6cbf08468e3fd64
Anton <Soloviev.gvardiya@gmail.com>
2023-05-28 18:23:43 +0300

    Initial commit

---
+++ src/test/java/com/example/skyprohomeworkalgorithmpart2/SkyproHomeworkAlgorithmPart2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.skyprohomeworkalgorithmpart2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkyproHomeworkAlgorithmPart2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
